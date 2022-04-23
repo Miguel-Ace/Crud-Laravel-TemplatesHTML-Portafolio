4b4ae898324350c6bacc365d78ef2b2a92155afd
Miguel <acevedo51198mac@gmail.com>
2022-04-22 21:17:29 -0600

    Creando el controlador del template

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProyectoController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
+
+Route::resource('/proyectos', ProyectoController::class);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
